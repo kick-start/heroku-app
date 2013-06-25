93f34afc3648affe68cb4a4a70ca4045d18c5e3b
OpenShift guest <gourav@aristontek.com>
2013-06-24 22:19:59 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuApp::Application.config.secret_token = '2c9e168610eaccc6784b142b028a1167b16957b7c7688ebcb8ab582abc23054b25f88506b8f514b1b4bfbf77f211fd57ba28fd135722b95be7be53604110f5e0'
